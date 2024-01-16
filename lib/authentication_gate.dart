1475f27320510c43870d32047d6f39d46dde0e80
Rama Astra <ramaastra333@gmail.com>
2024-01-16 19:51:13 +0800

    refactor (authpage): refactor AuthPage widget to be AuthenticationGate

--- lib/authentication_gate.dart
+++ lib/authentication_gate.dart
@@ -4,10 +4,10 @@ import 'package:mobile_app/pages/home.dart';
 import 'package:mobile_app/pages/login_or_register.dart';
 import 'package:mobile_app/services/authentication.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthenticationGate extends StatelessWidget {
   final AuthenticationService _authenticationService = AuthenticationService();
 
-  AuthPage({super.key});
+  AuthenticationGate({super.key});
 
   @override
   Widget build(BuildContext context) {
